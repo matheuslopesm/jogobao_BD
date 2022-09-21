a446ea9559746fd0ef00368d2d7203f7c1d5be95
Matheus Lopes <matheus.lopesmdev@gmail.com>
2022-09-21 00:14:09 -0300

    Criação de rotas no frontend (GET)

--- frontend/model/produto.js
+++ frontend/model/produto.js
@@ -2,6 +2,7 @@ const pnomeInput = document.getElementById("primaryName")
 const valueInput = document.getElementById("value")
 const companyInput = document.getElementById("company")
 const descriptionInput = document.getElementById("description")
+
 const btn = document.querySelector("#submitBtn")
 
 btn.addEventListener("click", (e) => {
@@ -13,7 +14,7 @@ btn.addEventListener("click", (e) => {
         empresa: companyInput.value,
         descricao: descriptionInput.value
     })
-   .then((response) => {
+    .then((response) => {
         console.log(response.data)
     });
 
@@ -30,98 +31,21 @@ function createProdutoOnRow(){
         <td>${companyInput.value}</td>
         <td>${descriptionInput.value}</td>
         <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
-        <td><a href="#" class="btn btn-info btn-sm edit">Edit</a></td>
+        <td><a href="#" class="btn btn-info btn-sm edit">Editar</a></td>
         `
-    list.appendChild(row);
+    
+    list.appendChild(row) 
 }
 
+async function showProdutos(){
+    let response = await axios.get("http://localhost:3000/produtos");
+    let data = response.data;
 
-// const creatingProduto = () => {
-//     axios.post("http://localhost:3000/produtos", {
-//         pnome: pnomeInput.value,
-//         valor: valueInput.value,
-//         empresa: companyInput.value,
-//         descricao: descriptionInput.value
-//     })
-//     .then((response) => {
-//         console.log(response.data)
-//     });
-// }
+    data.forEach(() => { 
+        createProdutoOnRow()
+    })
+}
 
-// const readingProduto = () => {
-//     axios.get('http://localhost:3000/produtos')
-//     .then(response => {
-//         const products = response.data.data;
-//         console.log(`GET list products`, products);
-//     })
-// }
-
-// const deletingProduto = (numeroserie) => {
-//     axios.delete(`http://localhost:3000/produtos/${numeroserie}`
-//         .then(response => {
-//             console.log(`DELETE: produto is removed`, numeroserie)
-//         }))
-// }
-
-// const updatingProduto = (atualizedProduto, numeroserie) => {
-//     axios.put(`http://localhost:3000/produtos/${numeroserie}`, atualizedProduto)
-//         .then(response => {
-//             console.log(response.data)
-//         })
-// }
-
-// btn.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     const pname = pnomeInput.value
-//     const value = valueInput.value
-//     const company = companyInput.value
-//     const description = descriptionInput.value
-
-//     postMethod(pname, value, company, description)
+showProdutos()
 
 
-// })
-
-// function postMethod(pname, value, company, description){
-
-// }
-
-// // function readMethod(){
-// //     axios.get("http://localhost:3000/produtos")
-// //         .then(response => {
-// //             console.log(response.data)
-// //         })
-// // }
-
-// // function atualizeMethod(){
-// //     axios.put(`http://localhost:3000/produtos/${numeroserie}`)
-// //         .then(response => {
-// //             console.log(response.data)
-// //         })
-// // }
-
-//     // const deleteProduto = (prod, numeroserie) => {
-//     //     axios.delete(`http://localhost:3000/produtos/${numeroserie}`)
-//     //         .then(response => {
-//     //             console.log(response.data)
-//     //         })
-//     // }
-
-// function showAlert(text, className){
-//     const div = document.createElement('div')
-
-//     div.className = `alert alert-${className}`
-
-//     div.appendChild(document.createTextNode(text))
-
-//     // Insere a mensagem de alerta antes do form
-//     const container = document.querySelector('.container')
-//     const form = document.querySelector('#printer-form')
-//     container.insertBefore(div, form)
-
-//     // Remove a mensagem após 3s
-//     setTimeout(() => document.querySelector('.alert').remove(), 3000)
-// }
-
-// function showProdutos(){
-//     const produtos = await )
