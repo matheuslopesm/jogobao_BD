17753bf0530d6af569da81c929ba2fa5c388ffbe
Matheus Lopes <matheus.lopesmdev@gmail.com>
2022-09-27 11:25:23 -0300

    Construção de rotas no frontend (PUT)

--- frontend/model/produto.js
+++ frontend/model/produto.js
@@ -21,16 +21,18 @@ btn.addEventListener("click", (e) => {
 
 function createProdutoOnRow(response){
     const list = document.querySelector('#product-list');
-    let produtos = response.data;   
+    var produtos = response.data;   
 
-    for(let produto of produtos) {
+    produtos.forEach(function (produto, i) {
+        console.log(`produto ${i}`, produto, i)
         const row = document.createElement('tr');
-        const numeroserie = document.createElement('th');
+
+        var numeroserie = document.createElement('th');
         const pnome = document.createElement('td');
         const valor = document.createElement('td');
         const empresa = document.createElement('td');
         const descricao = document.createElement('td');
-        const editButtons = document.createElement('td');
+        const edit = document.createElement('td');
 
         numeroserie.textContent = produto.numeroserie;
         pnome.textContent = produto.pnome;
@@ -43,14 +45,94 @@ function createProdutoOnRow(response){
         row.appendChild(valor);
         row.appendChild(empresa);
         row.appendChild(descricao);
-        row.appendChild(editButtons)
+        row.appendChild(edit);
+        // row.appendChild(editButtons);
 
-        editButtons.innerHTML = `
-        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
-        <td><a href="#" class="btn btn-info btn-sm edit">Editar</a></td>
-        `
+        // editButtons.innerHTML = `
+        // <td><a href="#" id="deleteBtn" class="btn btn-danger btn-sm delete">X</a></td>
+        // <td><a href="#" id="editBtn" class="btn btn-info btn-sm edit">Editar</a></td>
+        // `
+        
+        var editBtn = document.createElement('a');
+        editBtn.type = "button";
+        editBtn.classList.add(`editButton${produto.numeroserie}`, "btn", "btn-info", "btn-sm", "edit");
+        editBtn.textContent = "Editar";
+
+        editBtn.onclick = (function openModal() { 
+            document.querySelector(".modal").classList.add(`myModal${produto.numeroserie}`)
+            document.querySelector(`.myModal${produto.numeroserie}`).style.display = "block";
+            console.log(`modal [${produto.numeroserie}] abrindo`)
+
+            var pnomeInputModal = document.getElementById("primaryName_modal");
+            var valueInputModal = document.getElementById("value_modal");
+            var companyInputModal = document.getElementById("company_modal");
+            var descriptionInputModal = document.getElementById("description_modal");
+
+            pnomeInputModal.value = produto.pnome;
+            valueInputModal.value = produto.valor;
+            companyInputModal.value = produto.empresa;
+            descriptionInputModal.value = produto.descricao;
+
+            // axios.put(`http://localhost:3000/produtos/${produto.numeroserie}`, {
+            //     pnome: pnomeInputModal.value,
+            //     valor: valueInputModal.value,
+            //     empresa: companyInputModal.value,
+            //     descricao: descriptionInputModal.value
+            // })
+        });
+
+        edit.appendChild(editBtn);
         list.appendChild(row);
-    }
+
+        // function updateProdutoOnRow(){
+
+        //     // var submitModal = document.querySelector(".modal");
+
+        //     // submitModal.addEventListener("click", updateWithModal);
+
+        //     // for (var j = 0 ; j < submitModal.length; j++) {
+        //     //     submitModal[i].addEventListener("click", updateWithModal)
+        //     // }
+        // }
+
+        // function updateWithModal(){
+
+        //     var pnomeInputModal = document.getElementById("primaryName_modal");
+        //     var valueInputModal = document.getElementById("value_modal");
+        //     var companyInputModal = document.getElementById("company_modal");
+        //     var descriptionInputModal = document.getElementById("description_modal");
+
+        //     axios.put(`http://localhost:3000/produtos/${produto.numeroserie}`, {
+        //         pnome: pnomeInputModal.value,
+        //         valor: valueInputModal.value,
+        //         empresa: companyInputModal.value,
+        //         descricao: descriptionInputModal.value
+        //     })
+        //     .then((response) => {
+        //         console.log(response.data);  
+        //     });
+        // }
+    })
+
+    // produtos.forEach(function (produto, i) {
+
+    //     var updateBtn = document.getElementsByClassName("editBtn");
+
+    //     updateBtn[0].addEventListener("click", (e) => {});
+
+    //     var updateModal = document.querySelector(".modal");
+
+    //     for (var i = 0 ; i < updateBtn.length; i++) {
+    //         updateBtn[i].addEventListener("click" , (e) => {
+    //             updateModal.style.display = 'block'; 
+
+    //             document.querySelector("#primaryName_modal").value = produto.pnome;
+    //             document.querySelector("#value_modal").value = produto.valor;
+    //             document.querySelector("#company_modal").value = produto.empresa;
+    //             document.querySelector("#description_modal").value = produto.descricao;
+    //         }); 
+    //     } 
+    // })
 }
 
 axios
